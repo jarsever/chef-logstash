31f3ef702aea33593b30285fbcd5d85215f85bed
jarsever <jarsever@gmail.com>
2014-03-06 14:27:53 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -11,7 +11,7 @@ version          '0.8.0'
   supports os
 end
 
-%w{ build-essential runit git ant java logrotate python }.each do |ckbk|
+%w{ build-essential runit git java logrotate python }.each do |ckbk|
   depends ckbk
 end
 
